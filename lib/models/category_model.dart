411b080b141eba2bdd04b6f5cf6a21318cbece0a
Luwaga Micheal <luwagamicheal1@gmail.com>
2022-10-20 15:08:18 +0300

    add

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -16,4 +16,27 @@ class Category {
 
   @override
   List<Object?> get props => [id, name, image];
+
+  static List<Category> categories = [
+    Category(
+      id: 1,
+      name: 'Breakfast',
+      image: Image.asset('assets/breakfast1.jpeg'),
+    ),
+    Category(
+      id: 2,
+      name: 'Food',
+      image: Image.asset('assets/food1.jpg'),
+    ),
+    Category(
+      id: 3,
+      name: 'Saurce',
+      image: Image.asset('assets/saurce.jpg'),
+    ),
+    Category(
+      id: 4,
+      name: 'Drinks',
+      image: Image.asset('assets/drink1.jpeg'),
+    ),
+  ];
 }
